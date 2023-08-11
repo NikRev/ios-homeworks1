ebbdb347710733be06f9d043c2cc79d4755d053d
Nikita <nik007rev@gmail.com>
2023-08-11 12:53:50 +0300

    полностью готово

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -1,12 +1,12 @@
 import UIKit
 
-
 class ProfileView: UIView {
-    
+
     private let profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "profile_image")
         imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 3
         imageView.layer.borderColor = UIColor.white.cgColor
@@ -19,6 +19,8 @@ class ProfileView: UIView {
         label.text = "Hipster Cat"
         label.font = UIFont.boldSystemFont(ofSize: 18)
         label.textColor = .black
+        label.isUserInteractionEnabled = true
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
@@ -27,6 +29,8 @@ class ProfileView: UIView {
         label.text = "Waiting for something...."
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         label.textColor = .gray
+        label.isUserInteractionEnabled = true
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
@@ -35,6 +39,7 @@ class ProfileView: UIView {
         textField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         textField.textColor = .gray
         textField.placeholder = "Set your status...."
+        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.borderStyle = .roundedRect
         return textField
     }()
@@ -49,11 +54,12 @@ class ProfileView: UIView {
         button.layer.shadowRadius = 4
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOpacity = 0.7
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init() {
+        super.init(frame: .zero)
         setupSubviews()
         setupConstraints()
         setupGestureRecognizers()
@@ -73,37 +79,36 @@ class ProfileView: UIView {
     }
 
     private func setupConstraints() {
-        profileImageView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(16)
-            make.leading.equalToSuperview().offset(16)
-            make.width.equalTo(100)
-            make.height.equalTo(100)
-        }
+        NSLayoutConstraint.activate([
+            // Constraints for profileImageView
+            profileImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            profileImageView.widthAnchor.constraint(equalToConstant: 100),
+            profileImageView.heightAnchor.constraint(equalToConstant: 100),
 
-        nameLabel.snp.makeConstraints { make in
-            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
-            make.top.equalToSuperview().offset(30)
-        }
+            // Constraints for nameLabel
+            nameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 30),
+            //nameLabel.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            
 
-        statusLabel.snp.makeConstraints { make in
-            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
-            make.top.equalTo(nameLabel.snp.bottom).offset(8)
-            make.trailing.equalToSuperview().offset(-16)
-        }
+            // Constraints for statusLabel
+            statusLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            statusLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16),
+            statusLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
 
-        statusTextField.snp.makeConstraints { make in
-            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
-            make.top.equalTo(statusLabel.snp.bottom).offset(8)
-            make.trailing.equalToSuperview().offset(-100)
-            make.height.equalTo(30)
-        }
+            // Constraints for statusTextField
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 8),
+            statusTextField.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16),
+            statusTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -100),
+            statusTextField.heightAnchor.constraint(equalToConstant: 30),
 
-        submitButton.snp.makeConstraints { make in
-            make.top.equalTo(statusTextField.snp.bottom).offset(30)
-            make.leading.equalToSuperview().offset(16)
-            make.trailing.equalToSuperview().offset(-16)
-            make.height.equalTo(50)
-        }
+            // Constraints for submitButton
+            submitButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: 30),
+            submitButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            submitButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            submitButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
     private func setupGestureRecognizers() {
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -12,34 +12,32 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
     private var publications: [Publicantions] = []
 
     override func viewDidLoad() {
-            super.viewDidLoad()
-            tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "Cell")
-            view.addSubview(tableView)
-            tableView.rowHeight = UITableView.automaticDimension
-            tableView.estimatedRowHeight = 450 // Задайте начальное оценочное значение, но оно будет динамически скорректировано
-            constraintSetup()
-            tableView.delegate = self
-            tableView.dataSource = self
-            publications = Publicantions.make()
-           let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-               tapGesture.delegate = self
-               view.addGestureRecognizer(tapGesture)
-        }
-
-    @objc private func handleTap() {
-            // End editing on the text field to dismiss the keyboard
-            view.endEditing(true)
-        }
-
-    private func constraintSetup() {
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+        super.viewDidLoad()
+        
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 450 // Задайте начальное оценочное значение, но оно будет динамически скорректировано
+        setupConstraints()
+        tableView.delegate = self
+        tableView.dataSource = self
+        publications = Publicantions.make()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
     }
 
+    @objc private func handleTap() {
+        // End editing on the text field to dismiss the keyboard
+        view.endEditing(true)
+    }
+
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
 
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
@@ -48,7 +46,6 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Возвращаем количество строк в секции (количество публикаций)
         return publications.count
@@ -76,19 +73,14 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         return nil
     }
     
-    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         if section == 0 {
             return 220
         }
         return 0
     }
+
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-           
-           tableView.deselectRow(at: indexPath, animated: true)
-       }
-
-    
-    
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
-

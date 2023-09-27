29eb88d5b3c2eb7c97cffb6080ff13674e8e29da
Nikita <nik007rev@gmail.com>
2023-09-27 15:32:23 +0300

    доработать 2

--- Navigation/PhotosViewController.swift
+++ Navigation/PhotosViewController.swift
@@ -1,41 +1,86 @@
+// PhotosViewController.swift
 import UIKit
-import iOSIntPackage
 
-protocol ImagePublisherObserver {
-    func imagesDidUpdate(images: [UIImage])
-}
+class PhotosViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        // Рассчитываем ширину каждой ячейки в зависимости от количества столбцов (3 в данном случае)
+        let numberOfColumns: CGFloat = 3
+        let cellWidth = (UIScreen.main.bounds.width - (numberOfColumns - 1) * 10) / numberOfColumns
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth) // Высота равна ширине для квадратных ячеек
 
-class PhotosViewController: UIViewController, ImagePublisherObserver {
-    let imagePublisherFacade = ImagePublisherFacade()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
 
+        return collectionView
+    }()
+    
+    private var photoArray: [UIImage?] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Другая инициализация вашего контроллера...
-
-        // Регистрируем PhotosViewController как наблюдателя
-        imagePublisherFacade.subscribe(self)
-
-        // Начинаем добавлять изображения с интервалом каждые 0.5 секунды, повторяем более чем 10 раз
-        imagePublisherFacade.addImagesWithTimer(time: 0.5, repeat: 20) // Например, повторяем 20 раз
+        
+        title = "Photo Gallery"
+        view.addSubview(collectionView)
+        
+       
+        let photo1 = UIImage(named: "photo1")
+        let photo2 = UIImage(named: "photo2")
+        let photo3 = UIImage(named: "photo3")
+        let photo4 = UIImage(named: "photo4")
+        
+        
+        photoArray = [photo1, photo2, photo3, photo4]
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: "PhotoCell")
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // Скрываем NavigationBar при загрузке экрана
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
-
-    func imagesDidUpdate(images: [UIImage]) {
-        // Обработка полученных изображений, например, отображение их в интерфейсе
-        // Этот метод будет вызван, когда ImagePublisherFacade обновит изображения
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Здесь возвращаем количество фотографий в вашей коллекции
+        return photoArray.count
     }
-
-    deinit {
-        imagePublisherFacade.rechargeImageLibrary()
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotosCollectionViewCell
+        
+        // Здесь настраиваем ячейку коллекции с использованием фотографии из вашего массива
+        let photo = photoArray[indexPath.item] // Замените `yourPhotoArray` на ваш массив фотографий
+        cell.imageView.image = photo // Предполагается, что `imageView` - это `UIImageView` в вашей ячейке
+        
+        return cell
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Показываем NavigationBar перед отображением экрана
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Скрываем NavigationBar при уходе с экрана
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
 }
-
-// Расширение PhotosViewController для подписки на протокол ImageLibrarySubscriber
-extension PhotosViewController: ImageLibrarySubscriber {
-    func receive(images: [UIImage])  {
-        guard let firstImage = images.first else {
-            print("Получено пустое изображение.")
-            return
-        }
-    }
-}
-
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -23,22 +23,20 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
        }
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "Cell")
-            view.addSubview(tableView)
-            tableView.rowHeight = UITableView.automaticDimension
-            tableView.estimatedRowHeight = 450 // Задайте начальное оценочное значение, но оно будет динамически скорректировано
-            constraintSetup()
-            tableView.delegate = self
-            tableView.dataSource = self
-            publications = Publicantions.make()
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-            tapGesture.delegate = self
-            view.addGestureRecognizer(tapGesture)
-            tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "Cell")
-            tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "ProfileView")
+           super.viewDidLoad()
+           tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: "PhotosCell")
+           tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "Cell")
+           view.addSubview(tableView)
+           tableView.rowHeight = UITableView.automaticDimension
+           constraintSetup()
+           tableView.delegate = self
+           tableView.dataSource = self
+           publications = Publicantions.make()
+           let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+           tapGesture.delegate = self
+           view.addGestureRecognizer(tapGesture)
+       }
 
-    }
 
     public func updateHeaderView() {
         if let user = currentUser, let profileView = tableView.headerView(forSection: 0) as? ProfileView {
@@ -69,57 +67,79 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
 
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        // Возвращаем количество секций в таблице (1 для профиля)
-        return 1
-        
+        return 2
     }
-    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Возвращаем количество строк в секции (количество публикаций)
-        return publications.count
+        if section == 0 {
+            return 1
+        } else {
+            return publications.count
+        }
     }
+
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
-        let publication = publications[indexPath.row]
-        cell.configure(with: publication)
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        // Здесь устанавливаем высоту ячейки на основе содержимого
-        return 400
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PhotosCell", for: indexPath) as! PhotosTableViewCell
+            let photos = [UIImage(named: "photo1"), UIImage(named: "photo2"), UIImage(named: "photo3"), UIImage(named: "photo4")]
+            cell.configure(with: photos, indexPath: indexPath)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
+            let publication = publications[indexPath.row]
+            cell.configure(with: publication)
+            return cell
+        }
     }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-           if section == 0 {
-               let profileView = ProfileView()
-               let headerHeight: CGFloat = 220
-               profileView.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: headerHeight)
-               
-               // Отображаем информацию о пользователе в заголовке
-               if let user = currentUser {
-                   profileView.profileImageView.image = user.avatar
-                   profileView.nameLabel.text = user.userName
-               }
-               return profileView
-           }
-           return nil
-       }
-    
-    
+        if section == 0 {
+            let profileView = ProfileView()
+            return profileView
+        }
+        return nil
+    }
+
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         if section == 0 {
             return 220
+        } else if section == 1 {
+            return 100 //  высоту секции с фотографиями
+        } else {
+            return 0
         }
-        return 0
-    }
-  
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-           
-           tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    
+    // Динамический расчет высоты ячеек
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            // Высота ячеек с фотографиями должна быть достаточной для отображения всех фотографий
+            return UITableView.automaticDimension
+        } else {
+            // Высота ячеек с постами должна быть рассчитана на основе их содержимого
+            let publication = publications[indexPath.row]
+            // рассчитать высоту на основе текста и других элементов в ячейке
+            // Возвращаем минимальную высоту, чтобы избежать сворачивания
+            return max(400, estimatedHeightForPublication(publication))
+        }
+    }
+
+    // Функция для оценки высоты ячейки с постом на основе ее содержимого
+    private func estimatedHeightForPublication(_ publication: Publicantions) -> CGFloat {
+        // Рассчитайте высоту на основе содержимого вашей ячейки (текста, изображений и т. д.)
+        // Верните вычисленную высоту
+        return 400// Здесь возвращается минимальная высота в примере
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.section == 0 { // Проверяем, что нажата секция с фотографиями
+            let photosVC = PhotosViewController()
+            navigationController?.pushViewController(photosVC, animated: true) // Переходим на экран PhotosViewController
+        }
+        
+    }
 }
-
